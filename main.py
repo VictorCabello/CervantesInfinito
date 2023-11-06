95c8abc09859f33f7dd25031b9ca5b2f3bb50ecc
Victor Cabello <v.cabello.alvarez@accenture.com>
2023-11-06 17:13:07 -0600

    Descargamos la repilca del quijote como txt

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+The intention of this module is
+to create GPT implementation
+using all the Quijote as dataset
+"""
+from quijotedownloader import downloaded_quijote
+
+def main():
+    """
+    This funciton has the main flow
+    of te program
+    """
+    downloaded_quijote()
+
+if __name__ == '__main__':
+    main()
---
+++ quijotedownloader.py
@@ -0,0 +1,52 @@
+'''
+This module is created to download the
+entire series of books from Cervantes
+calle "El Quijote de La Mancha"
+'''
+
+from os import getcwd
+from os.path import join
+from os.path import exists
+from urllib import request
+
+target_folder = getcwd()
+'''
+This string represents the
+target directory where the
+book is downloaded
+'''
+
+QUIJOTE_FILE = 'el_quijote.txt'
+'''
+String that represents
+the file's name where the
+entire quijote books is
+stored
+'''
+
+QUIJOTE_URL = 'https://gist.githubusercontent.com/jsdario/6d6c69398cb0c73111e49f1218960f79/raw/8d4fc4548d437e2a7203a5aeeace5477f598827d/el_quijote.txt'
+'''
+URL to get the quijote
+'''
+
+quijote_path = join(target_folder, QUIJOTE_FILE)
+'''
+The file system path
+where the books are stored
+'''
+
+def is_there_quijote():
+    '''
+    True is there a file
+    on the quijote_path
+    '''
+    return exists(quijote_path)
+
+def downloaded_quijote():
+    '''
+    Download the quijote
+    as plain text
+    '''
+    if is_there_quijote():
+        return
+    request.urlretrieve(QUIJOTE_URL, quijote_path)
